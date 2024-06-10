04a817b04a34fbe7a056670a867f9e3fb73b682b
albertocj1 <albertochristianjoshua@gmail.com>
2024-06-11 01:40:58 +0800

    done log in

--- src/Controller/A_Login.java
+++ src/Controller/A_Login.java
@@ -6,11 +6,64 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class A_Login {
+
+    @FXML
+    private ImageView image1;
+
+    @FXML
+    private MediaView mediaView;
+
+    @FXML
+    TextField usernameField;
+
+    @FXML
+    PasswordField passwordField;
+
+    @FXML
+    public Text errorText;
+
+    public void initialize() {
+
+    }
+    
+    public void login(ActionEvent event) throws IOException {
+        String username = usernameField.getText().trim();
+        String password = passwordField.getText().trim();
+
+        if (username.equals("admin001") && password.equals("admin123!@#")) {
+
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getResource("/View/Admin-Dashboard.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } else if (username.equals("dept001") && password.equals("dept123!@#")) {
+
+            
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getResource("/View/Order_Dept.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } else {
+            errorText.setText("Invalid username or password. Please try again.");
+        }
+    }
 
     @FXML
     public void gotoAdmin(ActionEvent event) throws IOException {
